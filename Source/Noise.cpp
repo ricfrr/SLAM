9f3cd39724af68803f73cad1a162b2e175d972f6
MatteoTadiello <matteo.tadiello@studenti.unitn.it>
2019-01-16 17:09:03 +0100

    Add the noise in the point only for distances

---
+++ Source/Noise.cpp
@@ -0,0 +1,29 @@
+#include "../Headers/Noise.hpp"
+
+
+Noise::Noise(){
+    percentage=1;
+};
+Noise::~Noise(){
+};
+Noise::Noise(int percent){
+    percentage = percent/100;
+};
+
+void Noise::addNoise(pcl::PointCloud<pcl::PointXYZ>::Ptr cloudToModify){
+    std::mt19937 generatorDistance(std::random_device{}());
+    std::mt19937 generatorReflection(std::random_device{}());
+
+    for(auto& x : cloudToModify->points){
+        //Standard deviation given by the distance as factor of inclination
+        float stddev = x.z*inclination;
+        float reflection = 0.5;
+        //Generating the normal distribution for the errors
+        std::normal_distribution<float> dist(mean, stddev); //Distribution for distances
+        std::normal_distribution<float> rifl(mean, reflection);
+        //Generetaing the errors
+        float ErrDist= dist(generatorDistance);
+        float ErrRefl= rifl(generatorReflection);
+        x.z += (ErrDist+ErrRefl)*percentage;
+    }
+}
